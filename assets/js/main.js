e912cc77edf44d3a9d3e0f0c12ae72132449b587
Martin Pultrone <pmartinpultrone@gmail.com>
2021-09-03 14:49:13 -0300

    Portfolio terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,26 +75,38 @@ const modalViews = document.querySelectorAll('.services__modal'),
     modalBtns = document.querySelectorAll('.services__button'),
     modalCloses = document.querySelectorAll('.services__modal-close')
 
-let modal = function(modalClick){
+let modal = function (modalClick) {
     modalViews[modalClick].classList.add('active-modal')
 }
 
 modalBtns.forEach((modaLBtn, i) => {
-    modaLBtn.addEventListener('click', () =>{
+    modaLBtn.addEventListener('click', () => {
         modal(i)
     })
 })
 
 modalCloses.forEach((modalClose) => {
-    modalClose.addEventListener('click', () =>{
-        modalViews.forEach((modalView) =>{
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
             modalView.classList.remove('active-modal')
         })
     })
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
 
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
